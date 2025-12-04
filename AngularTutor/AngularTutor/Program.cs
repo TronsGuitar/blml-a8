1db62751cd365f866a5b54a21e355053ae9b8759
Brian A. Bass <6493295+TronsGuitar@users.noreply.github.com>
2025-12-03 21:37:17 -0600

    Add AngularTutor starter solution and guidance

---
+++ AngularTutor/AngularTutor/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AngularTutor API",
+        Version = "v1",
+        Description = "Starter API for Angular + ASP.NET Core tutorial",
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
